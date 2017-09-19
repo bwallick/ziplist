9431da4366f884009399212227d3e94fb0a95fc9
Blake Wallick <bwallick@hawaii.edu>
2017-09-18 20:46:53 -1000

    DNF

--- ziplist.js
+++ ziplist.js
@@ -1,2 +1,12 @@
+const testAlpha = ['a', 'b', 'c'];
+const testNumb = [1, 2, 3];
 
+function zipList(list1, list2) {
+  const resultList = [];
+  for (let i = 0; i < list1.length; i++) {
+    resultList(list1[i], list2[i]);
+  }
+  return resultList;
+}
 
+console.log(zipList(testAlpha, testNumb));
