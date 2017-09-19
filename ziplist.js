05e9bfd223bc72a3cce9aa06afe92efe331f9eec
Blake Wallick <bwallick@hawaii.edu>
2017-09-18 20:54:02 -1000

    def

--- ziplist.js
+++ ziplist.js
@@ -4,9 +4,15 @@ const testNumb = [1, 2, 3];
 function zipList(list1, list2) {
   const resultList = [];
   for (let i = 0; i < list1.length; i++) {
-    resultList(list1[i], list2[i]);
+    resultList.push(list1[i], list2[i]);
   }
   return resultList;
 }
 
 console.log(zipList(testAlpha, testNumb));
+
+function zipListTheSimpleWay(list1, list2) {
+  return _.flatten(_.zip(list1, list2));
+}
+
+console.log(zipListTheSimpleWay(testAlpha, testNumb));
